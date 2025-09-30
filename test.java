1cdbabcda6d80e4ed2f66a4545c97bb42f76ac73
shevaarza <shevaarza@gmail.com>
2025-09-30 09:41:39 +0700

    sheva tugas

--- test.java
+++ test.java
@@ -1,9 +1,12 @@
 public class test {
 
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
 
-        
+        System.out.println(" hai , ini baris kode pertama yang di modifikasi user 1 ");
+
+        System.out.println("Namaku sheva arzadina hizbullah kurniawan , dengan NIM ( 254107020173) , Hallo semua salam kenal ");
+
+
     }
 
 
