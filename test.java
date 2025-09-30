e1e37c14d4d771ddfcb13598f9cc79ecf0987fa6
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-09-30 09:49:48 +0700

    feat: add ringga

--- test.java
+++ test.java
@@ -5,7 +5,8 @@ public class test {
         System.out.println(" hai , ini baris kode pertama yang di modifikasi user 1 ");
 
         System.out.println("Namaku sheva arzadina hizbullah kurniawan , dengan NIM ( 254107020173) , Hallo semua salam kenal ");
-
+        System.out.println(("Hi, ini baris kode pertama yang dimodifikasi user 2"));
+        System.out.println("User 2 menambahkan baris baru");
 
     }
 
